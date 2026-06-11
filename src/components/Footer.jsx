aded1511224f4acc97f110b54c4c97297e395103
Abhinav <choudharyabhinav490@gmail.com>
2026-06-11 16:17:17 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className="relative z-10 border-t border-white/10 py-6 mt-12">
       <div className="container mx-auto px-6 text-center">
         <p className="text-gray-500 text-xs">
-          Images from Lorem Picsum • Page navigation with smooth loading
+         © 2024 Image Gallery • Powered by Lorem Picsum
         </p>
       </div>
     </div>
